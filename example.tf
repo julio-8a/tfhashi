0c6657372b6cfa45b901d89a8c7bd240b69e2fed
julio-8a <julioc8a@gmail.com>
2020-02-17 21:46:17 -0500

    Create instances, explicit dependencies, use count and element

--- example.tf
+++ example.tf
@@ -8,4 +8,35 @@ provider "aws" {
 resource "aws_instance" "hashiTraining" {
     ami           = var.amis[var.region]
     instance_type = var.free-tier-instance
+
+    tags = {
+        Name = "hashiTraining"
+    }
+
+    #Configure explicit dependency on S3 bucket creation
+    depends_on = [aws_s3_bucket.tf-s3-example]
+}
+
+#Resource to assing an elastic IP address to the EC2 instance
+resource "aws_eip" "ip" {
+    count    = var.instance_count
+    vpc      = true
+    instance = aws_instance.hashiTraining.id
+}
+
+#Create number of intances based on a variable
+resource "aws_instance" "hashiTrainingCount" {
+    count = var.instance_count
+    ami           = var.amis[var.region]
+    instance_type = var.free-tier-instance
+
+    tags = {
+        Name = element(var.instance_tags, count.index)
+    }
+}
+
+#new S3 resource 
+resource "aws_s3_bucket" "tf-s3-example" {
+    bucket = "tf-s3-example-20200217"
+    acl    = "private"
 }
--- variables.tf
+++ variables.tf
@@ -9,8 +9,18 @@ variable "amis" {
         "us-east-2" = "ami-0c55b159cbfafe1f0"
     }
 }
+
 variable "free-tier-instance" {
     description = "AWS instance type"
     default     = "t2.micro"
 }
 
+variable "instance_count" {
+    description = "Total instances for this build"
+    default     = 3
+}
+
+variable "instance_tags" {
+    type    = "list"
+    default = ["hashitraining-0", "hashitraining-1", "hashitraining-2"]
+}
