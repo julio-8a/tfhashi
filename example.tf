8289e2f564508bded4f1d81efb9c7630b5de5fa1
julio-8a <julioc8a@gmail.com>
2020-02-17 22:28:17 -0500

    Create key pair and configure provisioner. Failed to ssh

--- example.tf
+++ example.tf
@@ -4,6 +4,11 @@ provider "aws" {
     region  = var.region
 }
 
+#Create resource for ssh key-pair
+resource "aws_key_pair" "aws-key" {
+    key_name   = "joawskey"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
 #Leveraging free instance using a map type variable
 resource "aws_instance" "hashiTraining" {
     ami           = var.amis[var.region]
@@ -15,6 +20,28 @@ resource "aws_instance" "hashiTraining" {
 
     #Configure explicit dependency on S3 bucket creation
     depends_on = [aws_s3_bucket.tf-s3-example]
+
+    #Configure SSH key pair
+    connection {
+        type        = "ssh"
+        user        = "myec2-user"
+        private_key = file("~/.ssh/id_rsa")
+        host        = self.public_ip
+    }
+
+    #Provisioner to capture intance public IP address locally (local-exec)
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.hashiTraining.public_ip} > ip_address.txt"
+    }
+
+    #Provisioner to configure instance
+    provisioner "remote-exec" {
+        inline = [
+            "sudo amazon-linux-extras enable nginx1.12",
+            "sudo yum -y install nginx",
+            "sudo systemctl start nginx"
+        ]
+    }
 }
 
 #Resource to assing an elastic IP address to the EC2 instance
